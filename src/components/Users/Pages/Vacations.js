b1b3b0636da72b57cdc30b785658ece3752e3cfe
Ahmed Matter <matterahmed36@gmail.com>
2023-06-11 01:55:00 +0300

    update vacation page

--- src/components/Users/Pages/Vacations.js
+++ src/components/Users/Pages/Vacations.js
@@ -1,12 +1,31 @@
 import React, { useState } from 'react';
-import classes from './Home.module.css';
+import classes from './Vacations.module.css';
 import UserCard from '../Ui/UserCard';
-import TaskTaple from '../Ui/TaskTaple';
+import HeightLevelVecationTaple from '../Ui/HeightLevelVecationTaple';
+import LowLevelVacationsTaple from '../Ui/LowLevelVacationsTaple';
 import ActionCard from '../Ui/ActionCard';
 import AddTask from '../Ui/AddTask';
 import NavBar from '../Ui/NavBar';
 const Tasks = props => {
-  return <TaskTaple></TaskTaple>;
+  return (
+    <div className={classes.con}>
+      {props.userData.role === 'dean' ||
+      props.userData.role === 'head' ||
+      props.userData.role === 'vice' ? (
+        <>
+          <HeightLevelVecationTaple
+            userData={props.userData}
+          ></HeightLevelVecationTaple>
+        </>
+      ) : (
+        <>
+          <LowLevelVacationsTaple
+            userData={props.userData}
+          ></LowLevelVacationsTaple>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Tasks;
